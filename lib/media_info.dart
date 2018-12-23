45439467fd4d7ea7a551bbb8f225e64638e0d181
Sebastian Roth <sebastian.roth@gmail.com>
2018-12-23 17:41:07 +0800

    initial travis config, dart code only

--- lib/media_info.dart
+++ lib/media_info.dart
@@ -4,13 +4,13 @@ import 'package:flutter/services.dart';
 
 class MediaInfo {
   static const MethodChannel _channel =
-      const MethodChannel('asia.ivity.flutter/media_info');
+      MethodChannel('asia.ivity.flutter/media_info');
 
   /// Utilizes platform methods (which may include a combination of HW and SW decoders)
   /// to analyze the media file at a given path.
-  /// 
+  ///
   /// This will return *null* if the media file is invalid.
-  /// 
+  ///
   /// Valid media files will generate a dictionary with relevant fields set.
   /// For video files, this includes:
   /// - width
